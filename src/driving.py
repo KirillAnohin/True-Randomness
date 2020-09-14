fb248a01b1d3dab19f36a48b6e42afa351539964
Akiz09 <kirill.aa6@gmail.com>
2020-09-14 14:32:59 +0300

    Fixed code structure

--- src/driving.py
+++ src/driving.py
@@ -2,18 +2,17 @@ import time
 from threading import Thread
 import serial
 
+
 class SerialCom:
     def __init__(self, speed):
-        self.vastus = ""
         self.speed = speed
         self.ser = serial.Serial('/dev/ttyACM0', 115200, timeout=0.01)
         print(self.ser.name)
 
-
     def gs(self):
         self.ser.write('gs\r\n'.encode("utf-8"))
-        self.vastus += self.ser.read(19).decode("utf-8")
-        print(self.vastus)
+        response = self.ser.read(20).decode("utf-8")
+        print("Response message: ", response)
 
     # sd:vasak:taga:parem
     def forward(self):
@@ -23,10 +22,12 @@ class SerialCom:
         self.ser.write(('sd:' + str(-self.speed) + ':' + str(0) + ':' + str(self.speed) + '\r\n').encode("utf-8"))
 
     def left(self):
-        self.ser.write(('sd:' + str(-self.speed) + ':' + str(-self.speed) + ':' + str(-self.speed) + '\r\n').encode("utf-8"))
+        self.ser.write(
+            ('sd:' + str(-self.speed) + ':' + str(-self.speed) + ':' + str(-self.speed) + '\r\n').encode("utf-8"))
 
     def right(self):
-        self.ser.write(('sd:' + str(self.speed) + ':' + str(self.speed) + ':' + str(self.speed) + '\r\n').encode("utf-8"))
+        self.ser.write(
+            ('sd:' + str(self.speed) + ':' + str(self.speed) + ':' + str(self.speed) + '\r\n').encode("utf-8"))
 
     def __del__(self):
         self.ser.close()
