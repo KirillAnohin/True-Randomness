d42ca62c247a1f60d7201d5007b4f99135a88948
Akiz09 <kirill.aa6@gmail.com>
2020-09-26 18:33:13 +0300

    Added additional parameters for camConfig, also changed driving code

--- src/driving.py
+++ src/driving.py
@@ -1,45 +1,53 @@
 import threading
 import time
-from threading import Thread
 import serial
 
 
-class SerialCom:
+class serialCom:
 
     def commandThread(self):
         while self.running:
-            text = ("sd:" + str(self.speed[0]) + ":" + str(self.speed[1]) + ":" + str(self.speed[2]) + "\r\n")
-            time.sleep(0.05)
+            time.sleep(0.002)
             print(self.speed)
-            self.ser.write(text.encode("utf-8"))
+            self.ser.write(self.message.encode("utf-8"))
+            # Buffer cleanup
             while self.ser.inWaiting() > 0:
                 self.ser.read()
 
     def __init__(self):
         self.running = True
-        self.speed = [0, 0, 0]
+        self.message = ""
         self.w = threading.Thread(name='commandThread', target=self.commandThread)
         self.ser = serial.Serial('/dev/ttyACM0', 115200, timeout=0.01)
         print(self.ser.name)
         self.w.start()
+
+    def generateMessage(self, command, params=None):
+        if params is None:
+            self.message = command + "\r\n"
+        else:
+            self.message = command + ':' + ':'.join(params) + "\r\n"
 
     def gs(self):
         self.ser.write('gs\r\n'.encode("utf-8"))
         response = self.ser.read(20).decode("utf-8")
         return response
 
-    # sd:vasak:taga:parem
+    # sd:left:back:right
     def forward(self, speed):
-        self.speed = [speed, 0, -speed]
+        self.generateMessage("sd", [speed, 0, -speed])
 
     def reverse(self, speed):
-        self.speed = [-speed, 0, speed]
+        self.generateMessage("sd", [-speed, 0, speed])
 
     def left(self, speed):
-        self.speed = [-speed, -speed, -speed]
+        self.generateMessage("sd", [-speed, -speed, -speed])
 
     def right(self, speed):
-        self.speed = [speed, speed, speed]
+        self.generateMessage("sd", [speed, speed, speed])
+
+    def throw(self, speed):
+        self.generateMessage("d", speed)
 
     def __del__(self):
         self.running = False
--- src/main.py
+++ src/main.py
@@ -29,48 +29,13 @@ def manual_movment():
             print("AAAAAAAAA")
             obj1.left(10)
         elif k == ord("p"):
-            obj1.__del__()
             print("Break")
             break
 
-def conture():
+    obj1.__del__()
 
-    cv2.namedWindow("Processed")
-    image_thread = vision.imageCapRS2()
-
-    while True:
-        depth_frame, frame = image_thread.getFrame()
-
-        bilateral_filtered_image = cv2.bilateralFilter(frame, 5, 175, 175)
-
-        edge_detected_image = cv2.Canny(bilateral_filtered_image, 75, 200)
-
-        cv2.imshow('Edge', edge_detected_image)
-
-        contours, hierarchy = cv2.findContours(edge_detected_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-        contour_list = []
-        for contour in contours:
-            approx = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, True), True)
-            area = cv2.contourArea(contour)
-            if (len(approx) > 8) & (len(approx) < 23):
-                contour_list.append(contour)
-
-        cv2.drawContours(frame, contour_list, -1, (255, 0, 0), 2)
-
-        cv2.imshow('Processed', frame)
-
-        k = cv2.waitKey(1)
-        if k == ord("q"):
-            image_thread.setStopped(False)
-            break
-
-    cv2.destroyAllWindows()
-
-
+    
 if __name__ == "__main__":
     calibrate()
-    #manual_movment()
-    #conture()
-
+    manual_movment()
 
